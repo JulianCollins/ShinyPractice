d072f175bdcc5c5a4d00f55ff564c1ac6c9b2ada
JulianCollins <collins.julian@gmail.com>
2024-06-13 19:09:58 +0100

    upset js

---
+++ upset.R
@@ -0,0 +1,14 @@
+# upsetjs
+
+library(upsetjs)
+
+listInput <- list(one = c(1, 2, 3, 5, 7, 8, 11, 12, 13), two = c(1, 2, 4, 5, 10), three = c(1, 5, 6, 7, 8, 9, 10, 12, 13))
+
+
+upsetjs() %>% fromList(listInput) %>% interactiveChart()
+
+
+listInput <- list(A = c("a", "b"), B = c(sample(letters, 10)), three = c(sample(letters, 10)))
+
+
+upsetjs() %>% fromList(listInput) %>% interactiveChart()
